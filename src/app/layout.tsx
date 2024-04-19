108802b6b5abd767114ff61e5b07402ad3fc0b2e
awildan <awildan.firmansyah@gmail.com>
2024-04-19 21:27:53 +0700

    Move imageOg and description to constant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,12 @@ import { Source_Sans_3 } from "next/font/google";
 import "./globals.css";
 import Header from "~/components/header";
 import { ApolloWrapper } from "~/lib/graphql/apollo-wrapper";
+import { description, imageOg } from "~/lib/constant";
 
 const sourceSans = Source_Sans_3({
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
-
-const imageOg =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-73414.appspot.com/o/Og%20Image%2Fpokeverse.png?alt=media&token=f4622fc7-acd5-4a31-b7de-4d6efa6aff8c";
-const description = "Your digital safari into the wild realms of Pokémon! 🌟";
 
 export const metadata: Metadata = {
   title: "PokéVerse by Awil",
--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,1 +1,5 @@
 export const PER_PAGE = 40;
+export const imageOg =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-73414.appspot.com/o/Og%20Image%2Fpokeverse.png?alt=media&token=f4622fc7-acd5-4a31-b7de-4d6efa6aff8c";
+export const description =
+  "Your digital safari into the wild realms of Pokémon! 🌟";
